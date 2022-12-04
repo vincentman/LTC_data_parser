eb14125afaccef1a1839147ad4d64eef14dca9da
vincentkao <vincentman0403@gmail.com>
2022-12-04 22:23:07 +0800

    pretest finished

--- select_sample.py
+++ select_sample.py
@@ -7,45 +7,25 @@ import config
 
 if __name__ == '__main__':
     start = time.time()
+    data_pickle_path = path.join(config.data_serialized_path, config.data_serialized_file_name)
+    print('reading data pickle.....', data_pickle_path)
+    with open(data_pickle_path, 'rb') as handle:
+        data_pickle = pickle.load(handle)
+    data_df = pd.DataFrame.from_dict(data_pickle)
     file_name_years = os.listdir(config.sample_select_list_path)
+    data_all_years_samples_df = pd.DataFrame()
     for file_name_year in file_name_years:
-        # load serialize data pickle to df
-        year = file_name_year[0:3]
-        data_path_year = path.join(config.data_serialize_path, year, f'{year}_serialize.pickle')
-        if not path.exists(data_path_year):
-            # raise Exception(f'pickle not exist: {data_path_year}')
-            print(f'pickle not exist: {data_path_year}')
-        else:
-            print('reading pickle..... => ', data_path_year)
-            with open(data_path_year, 'rb') as handle:
-                data_pickle = pickle.load(handle)
-            data_df = pd.DataFrame.from_dict(data_pickle)
-            # load sample list
-            sample_list_file_path = path.join(config.sample_select_list_path, file_name_year)
-            print('reading sample list..... => ', sample_list_file_path)
-            sample_list_df = pd.read_excel(sample_list_file_path, engine='xlrd')  # use xlrd to read .xls file
-            sample_list = sample_list_df['案號']
-            sample_df = data_df[data_df['CASENO'].isin(sample_list)]
-            sample_pretest_df = sample_df[sample_df['PLAN_TYPE'] == '初評']
-            sample_posttest_df = sample_df[sample_df['PLAN_TYPE'] == '複評']
-            os.makedirs(path.join(config.data_sample_selected_path, year), exist_ok=True)
-            sample_data_pickle_pretest_path = path.join(config.data_sample_selected_path, year,
-                                                        f'{year}{config.sample_pretest_file_postfix}.pickle')
-            print('writing pickle with sample list(pretest..... => ', sample_data_pickle_pretest_path)
-            with open(sample_data_pickle_pretest_path,
-                      'wb') as handle:
-                pickle.dump(sample_pretest_df, handle, protocol=pickle.HIGHEST_PROTOCOL)
-                sample_pretest_df.to_excel(
-                    path.join(config.data_sample_selected_path, year,
-                              f'{year}{config.sample_pretest_file_postfix}.xlsx'), index=False)
-            sample_data_pickle_posttest_path = path.join(config.data_sample_selected_path, year,
-                                                         f'{year}{config.sample_posttest_file_postfix}.pickle')
-            print('writing pickle with sample list(posttest..... => ', sample_data_pickle_posttest_path)
-            with open(sample_data_pickle_posttest_path,
-                      'wb') as handle:
-                pickle.dump(sample_posttest_df, handle, protocol=pickle.HIGHEST_PROTOCOL)
-                sample_posttest_df.to_excel(
-                    path.join(config.data_sample_selected_path, year,
-                              f'{year}{config.sample_posttest_file_postfix}.xlsx'), index=False)
+        sample_list_file_path = path.join(config.sample_select_list_path, file_name_year)
+        print('reading sample list..... => ', sample_list_file_path)
+        sample_list_df = pd.read_excel(sample_list_file_path, engine='xlrd')  # use xlrd to read .xls file
+        sample_list = sample_list_df['案號']
+        data_one_year_samples_df = data_df[data_df['CASENO'].isin(sample_list)]
+        data_all_years_samples_df = pd.concat([data_all_years_samples_df, data_one_year_samples_df], axis=0)
+    with open(path.join(config.data_sample_selected_path, config.data_sample_selected_pickle_name),
+              'wb') as handle:
+        pickle.dump(data_all_years_samples_df, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    data_all_years_samples_df.to_excel(
+        path.join(config.data_sample_selected_path,
+                  config.data_sample_selected_xlsx_name), index=False)
     end = time.time()
     print('Elapsed time(sec) for select_sample: ', end - start)
--- serialize_data.py
+++ serialize_data.py
@@ -7,26 +7,30 @@ import config
 
 if __name__ == '__main__':
     start = time.time()
-    os.makedirs(config.data_serialize_path, exist_ok=True)
+    os.makedirs(config.data_serialized_path, exist_ok=True)
     dir_name_years = os.listdir(config.data_path)
+    all_years_df = pd.DataFrame()
     for dir_name_year in dir_name_years:
-        df_year = pd.DataFrame()
+        year_df = pd.DataFrame()
         file_name_months = os.listdir(path.join(config.data_path, dir_name_year))
-        os.makedirs(path.join(config.data_serialize_path, dir_name_year), exist_ok=True)
+        os.makedirs(path.join(config.data_serialized_path, dir_name_year), exist_ok=True)
         for file_name_month in file_name_months:
             file_path = path.join(config.data_path, dir_name_year, file_name_month)
             print('reading excel..... => ', file_path)
-            df_month = pd.read_excel(file_path, index_col=None, engine='openpyxl')
+            month_df = pd.read_excel(file_path, index_col=None, engine='openpyxl')
             # print(df.info())
             # print(df.describe())
-            df_year = pd.concat([df_year, df_month], axis=0)
+            year_df = pd.concat([year_df, month_df], axis=0)
         if len(file_name_months):
-            pickle_path = path.join(config.data_serialize_path, dir_name_year, f'{dir_name_year}_serialize.pickle')
+            pickle_path = path.join(config.data_serialized_path, dir_name_year, f'{dir_name_year}_serialized.pickle')
             print('writing pickle..... => ', pickle_path)
             with open(pickle_path,
                       'wb') as handle:
-                pickle.dump(df_year, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    # with open(join(config.data_serialize_path, f'all_serialize.pickle'), 'wb') as handle:
-    #     pickle.dump(df, handle, protocol=pickle.HIGHEST_PROTOCOL)
+                pickle.dump(year_df, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        all_years_df = pd.concat([all_years_df, year_df], axis=0)
+    pickle_path = path.join(config.data_serialized_path, config.data_serialized_file_name)
+    with open(pickle_path, 'wb') as handle:
+        print('writing pickle..... => ', pickle_path)
+        pickle.dump(all_years_df, handle, protocol=pickle.HIGHEST_PROTOCOL)
     end = time.time()
     print('Elapsed time(sec) for serialize_data: ', end - start)
