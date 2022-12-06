801f40e1f0c5f4def1bc79a03211ce0020a69324
vincent.kao <vincent.kao@cyweemotion.com>
2022-12-06 18:35:38 +0800

    merge sample list to pickle

--- select_sample.py
+++ select_sample.py
@@ -12,16 +12,19 @@ if __name__ == '__main__':
     with open(data_pickle_path, 'rb') as handle:
         data_pickle = pickle.load(handle)
     data_df = pd.DataFrame.from_dict(data_pickle)
+    data_case_no_df = data_df['CASENO'].apply(str)
     file_name_years = os.listdir(config.sample_select_list_path)
-    data_all_years_samples_df = pd.DataFrame()
+    all_sample_list_df = pd.DataFrame()
     for file_name_year in file_name_years:
         sample_list_file_path = path.join(config.sample_select_list_path, file_name_year)
         print('reading sample list..... => ', sample_list_file_path)
         sample_list_df = pd.read_excel(sample_list_file_path, engine='xlrd')  # use xlrd to read .xls file
-        sample_list = sample_list_df['案號']
-        data_case_no_df = data_df['CASENO'].apply(str)
-        data_one_year_samples_df = data_df[data_case_no_df.isin(sample_list)]
-        data_all_years_samples_df = pd.concat([data_all_years_samples_df, data_one_year_samples_df], axis=0)
+        all_sample_list_df = pd.concat([all_sample_list_df, sample_list_df], axis=0)
+    with open(path.join(config.sample_list_serialized_path, config.sample_list_pickle_name), 'wb') as handle:
+        pickle.dump(all_sample_list_df, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    all_sample_list = all_sample_list_df['案號']
+    all_sample_list.drop_duplicates(keep='first', inplace=True)
+    data_all_years_samples_df = data_df[data_case_no_df.isin(all_sample_list)]
     with open(path.join(config.data_sample_selected_path, config.data_sample_selected_pickle_name),
               'wb') as handle:
         pickle.dump(data_all_years_samples_df, handle, protocol=pickle.HIGHEST_PROTOCOL)
