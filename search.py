fd4d488f844a2ff06d58756e1fdb432431169e2c
vincent <vincentman0403@gmail.com>
2022-12-11 11:01:36 +0800

    add .py: 抓出有複評沒初評的資料

---
+++ search.py
@@ -0,0 +1,18 @@
+import config
+import pandas as pd
+import pickle
+import os.path as path
+import sys
+
+if sys.argv[1] == 'post_has_no_pre':
+    with open(path.join(config.data_sample_selected_path, config.data_sample_selected_pickle_name), 'rb') as handle:
+        all_df = pd.DataFrame.from_dict(pickle.load(handle))
+    all_df.drop_duplicates(inplace=True)
+    posttest_df = all_df[all_df['PLAN_TYPE'] == '複評']
+    posttest_df.drop_duplicates(subset=['CASENO'], keep='first', inplace=True)
+    post_has_pre_test_df = all_df[all_df['CASENO'].isin(posttest_df['CASENO']) & (all_df['PLAN_TYPE'] == '初評')]
+    post_has_pre_test_df.drop_duplicates(subset=['CASENO'], keep='first', inplace=True)
+    merged_df = pd.concat([posttest_df, post_has_pre_test_df], axis=0)
+    post_has_no_pre_df = merged_df.drop_duplicates(subset=['CASENO'], keep=False)
+    post_has_no_pre_df.to_excel(
+        '有複評沒初評.xlsx', index=False)
--- write_pickle_to_excel.py
+++ write_pickle_to_excel.py
@@ -14,16 +14,20 @@ if __name__ == '__main__':
         with open(data_pickle_path, 'rb') as handle:
             data_pickle = pickle.load(handle)
         data_df = pd.DataFrame.from_dict(data_pickle)
+        output_excel_path = path.join(config.data_serialized_path, config.data_serialized_xlsx_name)
+        print(f'write excel(count: {len(data_df)}).....{output_excel_path}')
         data_df.to_excel(
-            path.join(config.data_serialized_path, config.data_serialized_xlsx_name), index=False)
+            output_excel_path, index=False)
     elif sys.argv[1] == 'all_samples_data':
         data_pickle_path = path.join(config.data_sample_selected_path, config.data_sample_selected_pickle_name)
         print('reading data pickle.....', data_pickle_path)
         with open(data_pickle_path, 'rb') as handle:
             data_pickle = pickle.load(handle)
         data_df = pd.DataFrame.from_dict(data_pickle)
+        output_excel_path = path.join(config.data_sample_selected_path, config.data_sample_selected_xlsx_name)
+        print(f'write excel(count :{len(data_df)}).....{output_excel_path}')
         data_df.to_excel(
-            path.join(config.data_sample_selected_path, config.data_sample_selected_xlsx_name), index=False)
+            output_excel_path, index=False)
 
 end = time.time()
 print('Elapsed time(sec) for write_excel: ', end - start)
