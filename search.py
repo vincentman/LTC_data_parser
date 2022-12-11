9d7e5d7c92eb6158f3a6609a728521abc190150d
vincent <vincentman0403@gmail.com>
2022-12-11 16:44:56 +0800

    refactor

--- search.py
+++ search.py
@@ -3,7 +3,9 @@ import pandas as pd
 import pickle
 import os.path as path
 import sys
+import time
 
+start = time.time()
 if sys.argv[1] == 'post_has_no_pre':
     with open(path.join(config.data_sample_selected_path, config.data_sample_selected_pickle_name), 'rb') as handle:
         all_df = pd.DataFrame.from_dict(pickle.load(handle))
@@ -15,4 +17,6 @@ if sys.argv[1] == 'post_has_no_pre':
     merged_df = pd.concat([posttest_df, post_has_pre_test_df], axis=0)
     post_has_no_pre_df = merged_df.drop_duplicates(subset=['CASENO'], keep=False)
     post_has_no_pre_df.to_excel(
-        '有複評沒初評.xlsx', index=False)
+        path.join(config.data_output_tmp_path, '有複評沒初評.xlsx'), index=False)
+end = time.time()
+print('Elapsed time(sec) for search: ', end - start)
